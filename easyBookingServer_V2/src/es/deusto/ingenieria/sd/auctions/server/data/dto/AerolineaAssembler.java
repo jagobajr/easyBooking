b405c24d759e670dbcbc1a019ca673061ce6fc58
Jagoba Gomez <55583540+jagobajr@users.noreply.github.com>
2021-01-18 15:06:23 +0100

    Update AerolineaAssembler.java

--- easyBookingServer_V2/src/es/deusto/ingenieria/sd/auctions/server/data/dto/AerolineaAssembler.java
+++ easyBookingServer_V2/src/es/deusto/ingenieria/sd/auctions/server/data/dto/AerolineaAssembler.java
@@ -3,34 +3,39 @@ package es.deusto.ingenieria.sd.auctions.server.data.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-import es.deusto.ingenieria.sd.auctions.server.data.Category;
+import es.deusto.ingenieria.sd.auctions.server.data.Aerolinea;
+import es.deusto.ingenieria.sd.auctions.server.data.Aeropuerto;
 
 public class AerolineaAssembler {
-	private static AerolineaAssembler instance;
+    private static AerolineaAssembler instance;
 
-	private AerolineaAssembler() { }
-	
-	public static AerolineaAssembler getInstance() {
-		if (instance == null) {
-			instance = new AerolineaAssembler();
-		}
-		
-		return instance;
-	}
+    private AerolineaAssembler() { }
 
-	public AeropuertoDTO entityToDTO(Category category) {
-		AeropuertoDTO dto = new AeropuertoDTO();		
-		dto.setName(category.getName());
-		return dto;
-	}
+    public static AerolineaAssembler getInstance() {
+        if (instance == null) {
+            instance = new AerolineaAssembler();
+        }
 
-	public List<AeropuertoDTO> entityToDTO(List<Category> categories) {		
-		List<AeropuertoDTO> dtos = new ArrayList<>();
-		
-		for (Category category : categories) {
-			dtos.add(this.entityToDTO(category));
-		}
-		
-		return dtos;
-	}
+        return instance;
+    }
+
+
+    public AeropuertoDTO entityToDTO(Aeropuerto aeropuerto) {
+        AeropuertoDTO dto = new AeropuertoDTO();
+
+        dto.setNombre(aeropuerto.getNombre());
+        dto.setLocalidad(aeropuerto.getLocalidad());
+
+        return dto;
+    }
+    /*
+    public List<AeropuertoDTO> entityToDTO(List<Aerolinea> aerolinea) {
+        List<AeropuertoDTO> dtos = new ArrayList<>();
+
+        for (Aeropuerto aerolineas : aerolinea) {
+            dtos.add(this.entityToDTO(aerolineas));
+        }
+
+        return dtos;
+    }*/
 }
